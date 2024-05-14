8c5612262365d9a8886a2657453c4dc7d47e41bc
charlie.key <61072067+Mark-SmallCharlie@users.noreply.github.com>
2024-05-14 15:37:16 +0800

    Add files via upload

---
+++ 抽奖/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace 抽奖timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private int number;
+        private bool isRunning;
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Thread th = new Thread(ShowNumber);
+            th.Start();
+        }
+
+        private void ShowNumber()
+        { 
+            Random rnd = new Random();
+            isRunning=true;
+            while (isRunning)
+            {
+                number = rnd.Next(1, 101);
+                Dispatcher.Invoke(UpdateUI);
+                Thread.Sleep(100);
+            }
+        }
+       private void UpdateUI()
+        {
+            txtNumber.Text =number.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            isRunning=false;
+            MessageBox.Show("恭喜" + txtNumber.Text + "中奖！");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            isRunning = false;
+           
+        }
+    }
+}
