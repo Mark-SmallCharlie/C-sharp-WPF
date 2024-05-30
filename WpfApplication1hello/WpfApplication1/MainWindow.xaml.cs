e2ef1e184fa2deb6c53c9a4e1c15849ce46f2c73
charlie.key <61072067+Mark-SmallCharlie@users.noreply.github.com>
2024-05-30 23:02:25 +0800

    Add files via upload

---
+++ WpfApplication1hello/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LEDLibrary;
+using ComLibrary;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            player = new LEDPlayer("COM8");
+        }
+        private LEDPlayer player;
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string result = player.DisplayText(tb_LEDText.Text);
+            if (result != "")
+            {
+                MessageBox.Show(result);
+            }
+        }
+
+        
+    }
+}
