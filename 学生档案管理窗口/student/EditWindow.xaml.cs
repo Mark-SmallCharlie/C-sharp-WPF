03b81db678eb6a197cf327fea3fabdd0526100b9
charlie.key <61072067+Mark-SmallCharlie@users.noreply.github.com>
2024-06-19 00:30:23 +0800

    Add files via upload

---
+++ 学生档案管理窗口/student/EditWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace student名单
+{
+    /// <summary>
+    /// EditWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        public EditWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Student student;
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            student = new Student();
+            student.Class=txtClass.Text;
+            student.Name = txtName.Text;
+            student.ID=txtId.Text;
+            student.Gender = txtGender.Text;
+
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ 学生档案管理窗口/student/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Formats.Asn1;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace student名单
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            liview.ItemsSource = students;
+        }
+
+        private List<Student> students = new List<Student>();
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            //Student stu = new Student();
+            //stu.Class = "321";
+            //stu.ID = "123";
+            //stu.Name = "231";
+            //stu.Gender = "213";
+            //students.Add(stu);
+            //liview.Items.Refresh();
+            EditWindow edit = new EditWindow();
+            edit.ShowDialog();
+            if (edit.student != null)
+            {
+                students.Add(edit.student);
+                liview.Items.Refresh();
+            }
+
+        }
+
+        private void btnedit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
