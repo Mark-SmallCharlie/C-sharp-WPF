7708de65cebe86ab92adcdfa9ab40e299005bc86
charlie.key <61072067+Mark-SmallCharlie@users.noreply.github.com>
2024-06-23 21:34:54 +0800

    Add files via upload

---
+++ 考核2/FileStorage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace 考核2
+{
+    internal class FileStorage
+    {
+
+        //判断文件是否存在，不存在则创建
+        public FileStorage(string path)        // 构造函数
+        {
+            file = new FileInfo(path);         // 创建文件对象
+            // 如果文件不存在，则创建
+            if (!file.Exists)
+            {
+                file.CreateText().Close();
+            }
+        }
+        private FileInfo file;
+
+        public void Add(string content)
+        {
+            StreamWriter sw = file.AppendText();
+            sw.WriteLine(content);
+            sw.Close();
+        }
+        public void open()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = file.FullName,
+                    UseShellExecute = true // 对于文件来说，通常应该为true  
+                });
+            }
+            catch (Exception ex)
+            {
+                // 捕获异常并处理  
+                MessageBox.Show("无法打开文件：" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            //System.Diagnostics.Process.Start(file.FullName);
+        }
+    }
+}
---
+++ 考核2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+using System.Timers;
+using System.IO;
+
+
+namespace 考核2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval =TimeSpan.FromSeconds(1);
+            timer.Tick += ShowRandomNum;
+        }
+
+
+        private void ShowRandomNum(object? sender, EventArgs e)
+        {
+            Random random = new Random();
+            int num = random.Next(1, 100);
+            txt1.Text = num.ToString();
+        }
+
+        private FileStorage fileStorage = 
+                new FileStorage(@"D:\Users\XKY\Documents\AA学习小组PPT等文件\C# WPF\考核2\考核2\Numbers.txt");
+        private FileInfo file;
+        private StreamWriter writer;
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // 开启计时器
+            timer.Start();           
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            // 停止计时器
+            timer.Stop();
+            // 将数字写入文件
+            fileStorage.Add(txt1.Text);
+            fileStorage.open();
+            
+        }
+    }
+}
